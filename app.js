61095c118d0cc269b1d2c9785c89d3cd3b3c73aa
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-07-31 15:13:11 +0200

    ok

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
-
-app.use('/api/auth', authRouter) // Роутер для авторизации
+app.use('/users', authRouter); // Роутер для авторизации начинается с users
+// app.use('/api/auth', authRouter) // Роутер для авторизации
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
